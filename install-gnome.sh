c5c7300026813274f37a31379b43bc8f4910ce62
MarkWolf257 <49902391+MarkWolf257@users.noreply.github.com>
2024-08-24 20:54:59 +0530

    Update install-gnome.sh

--- install-gnome.sh
+++ install-gnome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm xorg-server gnome-shell gdm alacritty gnome-control-center gnome-shell-extensions gnome-tweaks gnome-shell-extension-caffeine gnome-shell-extension-vitals ttf-jetbrains-mono-nerd neovim
+sudo pacman -Syu --noconfirm xorg-server gnome-shell gdm alacritty gnome-control-center gnome-shell-extensions gnome-tweaks gnome-shell-extension-caffeine gnome-shell-extension-vitals ttf-jetbrains-mono-nerd neovim starship
 
 mkdir AURs
 cd AURs
@@ -15,14 +15,30 @@ do
 	cd ..
 done
 
-cd ..
-
 extensions=("user-theme@gnome-shell-extensions.gcampax.github.com" "blur-my-shell@aunetx" "caffeine@patapon.info" "Vitals@CoreCoding.com" "forge@jmmaranan.com") 
 
 for extension in ${extensions[@]}
 do
 	gnome-extensions enable $extension
 done
+
+mkdir ~/.config/alacritty
+cp alacritty.toml ~/.config/alacritty/alacritty.toml
+
+starship preset nerd-font-symbols -o ~/.config/starship.toml
+
+git clone https://github.com/NvChad/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
+cd Colloid-gtk-theme
+./install.sh --tweaks $1 --theme $2 --color $3
+cd ..
+
+git clone https://github.com/vinceliuice/Colloid-icon-theme.git
+cd Colloid-icon-theme
+./install.sh --scheme $1 --theme $2 --color $3
+cd ..
 
 sudo systemctl enable gdm.service
 reboot
