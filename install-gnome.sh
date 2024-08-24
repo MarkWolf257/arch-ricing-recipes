14e565a3e1cc9ebffbfb625a29dc0d6d9f142eed
MarkWolf257 <49902391+MarkWolf257@users.noreply.github.com>
2024-08-24 18:53:58 +0000

    Add files via upload

--- install-gnome.sh
+++ install-gnome.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm xorg-server gnome-shell gdm alacritty gnome-control-center gnome-shell-extensions gnome-tweaks gnome-shell-extension-caffeine gnome-shell-extension-vitals ttf-jetbrains-mono-nerd neovim starship
+sudo pacman -Syu --noconfirm xorg-server gnome-shell gdm alacritty gnome-control-center gnome-shell-extensions gnome-tweaks gnome-shell-extension-caffeine gnome-shell-extension-vitals ttf-jetbrains-mono-nerd neovim starship nemo
 
 mkdir AURs
 cd AURs
 
-aurs=("text-engine" "extension-manager" "gnome-shell-extension-blur-my-shell" "gnome-shell-extension-forge" "zen-browser-optimized-bin")
+aurs=("text-engine" "extension-manager" "gnome-shell-extension-blur-my-shell" "gnome-shell-extension-forge" "zen-browser-bin")
 
 for aur in ${aurs[@]}
 do
@@ -14,6 +14,18 @@ do
 	makepkg --noconfirm -si
 	cd ..
 done
+
+git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
+cd Colloid-gtk-theme
+./install.sh --tweaks $1 --theme $2 --color $3
+cd ..
+
+git clone https://github.com/vinceliuice/Colloid-icon-theme.git
+cd Colloid-icon-theme
+./install.sh --scheme $1 --theme $2
+cd ..
+
+cd ..
 
 extensions=("user-theme@gnome-shell-extensions.gcampax.github.com" "blur-my-shell@aunetx" "caffeine@patapon.info" "Vitals@CoreCoding.com" "forge@jmmaranan.com") 
 
@@ -25,20 +37,11 @@ done
 mkdir ~/.config/alacritty
 cp alacritty.toml ~/.config/alacritty/alacritty.toml
 
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
 starship preset nerd-font-symbols -o ~/.config/starship.toml
 
 git clone https://github.com/NvChad/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
-
-git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
-cd Colloid-gtk-theme
-./install.sh --tweaks $1 --theme $2 --color $3
-cd ..
-
-git clone https://github.com/vinceliuice/Colloid-icon-theme.git
-cd Colloid-icon-theme
-./install.sh --scheme $1 --theme $2 --color $3
-cd ..
 
 sudo systemctl enable gdm.service
 reboot
